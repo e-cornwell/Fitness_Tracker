1bf670b8f0ff7c70c7d1ef90af789febfc5d3541
jwalus <walusjacob@gmail.com>
2023-02-06 12:19:38 -0600

    API Users, one passing, few errors

--- api/users.js
+++ api/users.js
@@ -1,11 +1,42 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
-const router = express.Router();
+const usersRouter = express.Router();
 const { getUserByUsername, createUser } = require('../db')
-
+const jwt = require('jsonwebtoken');
 // POST /api/users/register
+usersRouter.post('/register', async (req, res, next) => {
 
+    try {
+        const { username, password } = req.body;
+        const _user = await getUserByUsername(username);
+        const user = await createUser({ username, password });
+        if (!user) {
+            next({
+                name: "UserNotCreated",
+                message: "User not created"
+            });
+        } else if (_user) {
+            next({
+                name: "UserAlreadyExists",
+                message: "User already exists"
+            });
+        } else if (password.length < 8){
+            next({
+                name: "PasswordMustBeGreaterThenEightCharacters",
+                message: "Password must be greater then eight characters"
+            });
+        } else {
+            const token = jwt.sign({ id: user.id, username: user.username}, process.env.JWT_SECRET);
+            res.send({ user, message: "User created", token });
+        } 
 
+        
+
+    } catch (error) {
+    throw error;
+    }
+
+});
 
 // POST /api/users/login
 
@@ -13,4 +44,4 @@ const { getUserByUsername, createUser } = require('../db')
 
 // GET /api/users/:username/routines
 
-module.exports = router;
+module.exports = usersRouter;
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-
+app.use(express.json())
 // Setup your Middleware and API Router here
 const cors = require('cors')
 app.use(cors());
