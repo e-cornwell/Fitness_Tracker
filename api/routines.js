7e82973213678fdd9ca016ec9881a1d09a376d84
jwalus <walusjacob@gmail.com>
2023-02-08 10:15:33 -0600

    routines api two out of five functions working

--- api/routines.js
+++ api/routines.js
@@ -1,9 +1,47 @@
 const express = require('express');
-const router = express.Router();
+const routinesRouter = express.Router();
+const { createRoutine, getAllPublicRoutines } = require('../db')
+const jwt = require('jsonwebtoken');
 
 // GET /api/routines
 
+routinesRouter.get('/', async(req, res, next) => {
+    
+    const routines = await getAllPublicRoutines();
+    res.send(routines);
+
+});
+
 // POST /api/routines
+
+routinesRouter.post('/', async(req, res, next) => {
+    const { isPublic, name, goal } = req.body;
+    const auth = req.header("Authorization");
+
+    try {
+        if(!auth) {
+            res.send({
+                error: "An Error Message",
+                message: "You must be logged in to perform this action",
+                name: "Name"
+            });
+        } else {
+            const token = auth.slice(7);
+            const user = jwt.verify(token, process.env.JWT_SECRET);
+            const creatorId = user.id;
+            const newRoutine = await createRoutine({ 
+                creatorId, 
+                isPublic, 
+                name, 
+                goal });
+            res.send(newRoutine);
+        }
+
+    } catch (error) {
+        throw error;
+    }
+
+});
 
 // PATCH /api/routines/:routineId
 
@@ -11,4 +49,4 @@ const router = express.Router();
 
 // POST /api/routines/:routineId/activities
 
-module.exports = router;
+module.exports = routinesRouter;
