c14b289d43650790d4bc01338df162202d046607
Ethan Cornwell <ethan.aubrey22@gmail.com>
2023-02-09 11:01:02 -0700

    Delete routines_activities in process

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { updateRoutineActivity, getRoutineActivityById } = require('../db')
+const { updateRoutineActivity, getRoutineActivityById, destroyRoutineActivity } = require('../db')
 const jwt = require('jsonwebtoken');
 
 // PATCH /api/routine_activities/:routineActivityId
@@ -10,7 +10,7 @@ router.patch('/:routineActivityId', async (req, res, next) => {
     const { count, duration } = req.body;
     const { routineActivityId } = req.params;
     const auth = req.header("Authorization");
-    console.log(routineActivityId);
+    
     if (!auth) {
         res.send({
             error: "Error",
@@ -22,7 +22,7 @@ router.patch('/:routineActivityId', async (req, res, next) => {
         try {
             const { id, username } = jwt.verify(token, process.env.JWT_SECRET);
             let getRoutine = await getRoutineActivityById(routineActivityId);
-            console.log(getRoutine);
+            
             if (id === getRoutine.id) {
                 getRoutine = await updateRoutineActivity({ id: routineActivityId, count, duration });
                 res.send(getRoutine);
@@ -41,8 +41,39 @@ router.patch('/:routineActivityId', async (req, res, next) => {
 
 });
 
-
-
 // DELETE /api/routine_activities/:routineActivityId
+
+router.delete('/:routineActivityId', async (req, res, next) => {
+    const { routineActivityId } = req.params;
+    const auth = req.header("Authorization");
+    
+    if (!auth) {
+        res.send({
+            error: "Error",
+            message: "You must be logged in to perform this action",
+            name: "Error"
+        });
+    } else if (auth) {
+        const token = auth.slice(7);
+        try {
+            const { id, username } = jwt.verify(token, process.env.JWT_SECRET);
+            let getRoutineActivity = await getRoutineActivityById(routineActivityId);
+            
+            if (id === getRoutineActivity.routineId) {
+                await destroyRoutineActivity(getRoutineActivity.routineId);
+                res.send(getRoutineActivity)
+            } else {
+                res.status(403).send({
+                    error: "Error",
+                    message: `User ${username} is not allowed to delete In the afternoon`,
+                    name: "Error"
+                })
+            }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+})
 
 module.exports = router;
