97016859b57926ad2367ac7708ba6b3e5b6bf984
Ethan Cornwell <ethan.aubrey22@gmail.com>
2023-02-09 14:37:37 -0700

    POST /api/activities passing 1/2 tests

--- api/activities.js
+++ api/activities.js
@@ -1,21 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getAllActivities } = require('../db')
-const jwt = require('jsonwebtoken');
+const { getAllActivities, createActivity } = require("../db");
+const jwt = require("jsonwebtoken");
 
 // GET /api/activities
 
-router.get('/', async(req, res, next) => {
-    
+router.get("/", async (req, res, next) => {
+  try {
     const activities = await getAllActivities();
     res.send(activities);
-
+  } catch (error) {
+    console.log(error);
+  }
 });
-
-// GET /api/activities/:activityId/routines
 
 // POST /api/activities
 
+router.post("/", async (req, res, next) => {
+  try {
+    const newActivity = await createActivity(req.body);
+    console.log(newActivity);
+
+    if (newActivity.name !== undefined) {
+      res.send(newActivity);
+    }
+    res.send({
+      error: "Any<String>",
+      message: `An activity with name ${req.body.name} already exists`,
+      name: "Any<String>"
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 // PATCH /api/activities/:activityId
+
+// GET /api/activities/:activityId/routines
 
 module.exports = router;
